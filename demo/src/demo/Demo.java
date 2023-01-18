bd5c82fb083fff2bfae1ab2c7aa384723e8131ec
ulicesp <ulicesp857@gmail.com>
2023-01-17 20:03:37 -0600

    version1 java -MEZCLA MASTER

---
+++ demo/src/demo/Demo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo;
+
+public class Demo {
+
+
+    
+    
+    /*
+titulo = git 
+cuerpo :
+git ,merge --no--ff
+   git diff
+*/
+    
+}
